8710f57d6b086480859c29c5ea1eb407a62ff8f3
Aseel <asilalhashmi@email.com>
2026-05-16 12:28:33 +0700

    Create manusia composition structure

--- CompositionProject/manusia.h
+++ CompositionProject/manusia.h
@@ -1,1 +1,24 @@
-#pragma once
+#ifndef MANUSIA_H
+#define MANUSIA_H
+
+#include <iostream>
+#include <string>
+
+#include "jantung.h"
+
+using namespace std;
+
+class manusia {
+
+public:
+
+    string nama;
+
+    jantung varJantung;
+
+    manusia(string pNama);
+
+    ~manusia();
+};
+
+#endif
