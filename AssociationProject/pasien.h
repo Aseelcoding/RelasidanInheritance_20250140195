d9fd4c075d71faa7d42be315f5c825882fe336c7
Aseel <asilalhashmi@email.com>
2026-05-16 12:21:24 +0700

    Create pasien header with forward declaration

--- AssociationProject/pasien.h
+++ AssociationProject/pasien.h
@@ -1,1 +1,28 @@
-#pragma once
+#ifndef PASIEN_H
+#define PASIEN_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class dokter;
+
+class pasien {
+
+public:
+
+    string nama;
+
+    vector<dokter*> daftar_dokter;
+
+    pasien(string pNama);
+
+    ~pasien();
+
+    void tambahDokter(dokter* pDokter);
+
+    void cetakDokter();
+};
+
+#endif
