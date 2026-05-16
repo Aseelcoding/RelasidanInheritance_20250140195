afc0b15773872f48d8fcef3675fbba36022ed4d5
Aseel <asilalhashmi@email.com>
2026-05-16 12:39:46 +0700

    edit

--- AggregationProject/ibu.h
+++ AggregationProject/ibu.h
@@ -2,27 +2,44 @@
 #define IBU_H
 
 #include <iostream>
+using namespace std;
 #include <vector>
-
 #include "anak.h"
 
-using namespace std;
-
 class ibu {
-
 public:
 
     string nama;
-
     vector<anak*> daftar_anak;
 
-    ibu(string pNama);
+    ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
 
-    ~ibu();
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
 
-    void tambahAnak(anak* pAnak);
-
+    void tambahAnak(anak*);
     void cetakAnak();
+
 };
 
+void ibu::tambahAnak(anak* pAnak) {
+    daftar_anak.push_back(pAnak);
+}
+
+void ibu::cetakAnak() {
+    cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
+
+    /*for (auto& a : daftar_anak) {
+        cout << a->nama << "\n";
+    }*/
+
+    for (int i = 0; i < daftar_anak.size(); i++) {
+        cout << daftar_anak[i]->nama << endl;
+    }
+
+    cout << endl;
+}
 #endif
