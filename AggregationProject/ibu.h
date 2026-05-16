eaef5101f5c8fb04ce8bfd95cea008a5e4022583
Aseel <asilalhashmi@email.com>
2026-05-16 12:18:57 +0700

    Create ibu header and aggregation structure

--- AggregationProject/ibu.h
+++ AggregationProject/ibu.h
@@ -1,1 +1,28 @@
-#pragma once
+#ifndef IBU_H
+#define IBU_H
+
+#include <iostream>
+#include <vector>
+
+#include "anak.h"
+
+using namespace std;
+
+class ibu {
+
+public:
+
+    string nama;
+
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama);
+
+    ~ibu();
+
+    void tambahAnak(anak* pAnak);
+
+    void cetakAnak();
+};
+
+#endif
