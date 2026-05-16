6ec26d81b388b6ced551df4f1d7ffa13d05a5daf
Aseel <asilalhashmi@email.com>
2026-05-16 12:28:06 +0700

    Create jantung class

--- CompositionProject/jantung.h
+++ CompositionProject/jantung.h
@@ -1,1 +1,23 @@
-#pragma once
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+#include <iostream>
+
+using namespace std;
+
+class jantung {
+
+public:
+
+    jantung()
+    {
+        cout << "jantung dihidupkan\n";
+    }
+
+    ~jantung()
+    {
+        cout << "jantung dimatikan\n";
+    }
+};
+
+#endif
