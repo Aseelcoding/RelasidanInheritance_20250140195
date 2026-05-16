86b55bb900ab38c3a029e34de7975da582dc3e07
Aseel <asilalhashmi@email.com>
2026-05-16 12:25:07 +0700

    Create base class orang

--- InheritanceProject/orang.h
+++ InheritanceProject/orang.h
@@ -1,1 +1,22 @@
-#pragma once
+#ifndef ORANG_H
+#define ORANG_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class orang {
+
+public:
+
+    string nama;
+
+    orang(string pNama);
+
+    ~orang();
+
+    int jumlah(int a, int b);
+};
+
+#endif
