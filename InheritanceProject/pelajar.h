3e1527f3a43c0d84bc4edf955146d1c51386000c
Aseel <asilalhashmi@email.com>
2026-05-16 12:26:26 +0700

    Create derived class pelajar

--- InheritanceProject/pelajar.h
+++ InheritanceProject/pelajar.h
@@ -1,1 +1,20 @@
-#pragma once
+#ifndef PELAJAR_H
+#define PELAJAR_H
+
+#include "orang.h"
+
+class pelajar : public orang {
+
+public:
+
+    string sekolah;
+
+    pelajar(string pNama,
+        string pSekolah);
+
+    ~pelajar();
+
+    string perkenalan();
+};
+
+#endif
