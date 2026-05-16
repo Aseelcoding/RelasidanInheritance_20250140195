ee53ca196d0872c6071ffb7d830b4b76386fcd88
Aseel <asilalhashmi@email.com>
2026-05-16 12:22:05 +0700

    Create dokter header and association structure

--- AssociationProject/dokter.h
+++ AssociationProject/dokter.h
@@ -1,1 +1,28 @@
-#pragma once
+#ifndef DOKTER_H
+#define DOKTER_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class pasien;
+
+class dokter {
+
+public:
+
+    string nama;
+
+    vector<pasien*> daftar_pasien;
+
+    dokter(string pNama);
+
+    ~dokter();
+
+    void tambahPasien(pasien* pPasien);
+
+    void cetakPasien();
+};
+
+#endif
