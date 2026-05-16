07f389c4f27e5a4dc8619019896eeb5ffefd7d81
Aseel <asilalhashmi@email.com>
2026-05-16 12:23:56 +0700

    Complete association example and bidirectional relationships

--- AssociationProject/main.cpp
+++ AssociationProject/main.cpp
@@ -1,11 +1,43 @@
-//
 #include <iostream>
-using namespace std;
+
 #include "dokter.h"
 #include "pasien.h"
-int main()
-{
-    std::cout << "Hello World!\n";
+
+using namespace std;
+
+int main() {
+
+    dokter* varDokter1 =
+        new dokter("dr.Budi");
+
+    dokter* varDokter2 =
+        new dokter("dr.Tono");
+
+    pasien* varPasien1 =
+        new pasien("Andi");
+
+    pasien* varPasien2 =
+        new pasien("Lia");
+
+    varDokter1->tambahPasien(varPasien1);
+
+    varDokter1->tambahPasien(varPasien2);
+
+    varDokter2->tambahPasien(varPasien1);
+
+    varDokter1->cetakPasien();
+
+    varDokter2->cetakPasien();
+
+    varPasien1->cetakDokter();
+
+    varPasien2->cetakDokter();
+
+    delete varPasien1;
+    delete varPasien2;
+
+    delete varDokter1;
+    delete varDokter2;
+
+    return 0;
 }
-
-
