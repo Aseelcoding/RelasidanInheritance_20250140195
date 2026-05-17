a6d8bf6747a391685b53dece405bd38f266da23d
Aseel <asilalhashmi@email.com>
2026-05-17 14:27:39 +0700

    Create User Base Class

--- UserManagementOOP/UserManagementOOP.cpp
+++ UserManagementOOP/UserManagementOOP.cpp
@@ -3,6 +3,35 @@
 #include <iostream>
 using namespace std;
 
+class User 
+{
+	static int GlobalId;
+	int ID = 0;
+	string Name;
+	string Email;
+
+	User(string Name, string Email) 
+	{
+		this->Email = Email;
+		this->Name = Name;
+		this->ID = GenerateId();
+	}
+	
+	int GenerateId() 
+	{
+		return ++GlobalId;
+	}
+
+	int GetId() 
+	{
+		return ID;
+	}
+
+	string GetName() { return Name; }
+
+	string GetEmail() { return Email; };
+};
+int User::GlobalId = 0;
 
 int main()
 {
